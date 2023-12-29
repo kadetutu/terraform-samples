dbf54e1bbb3b74aa810e1c6b763a343e51097d0f
Kenny Adetutu <kenny.adetutu@gmail.com>
2023-12-29 11:02:20 -0500

    creating vpc configuration

--- ktech-infra/variables.tf
+++ ktech-infra/variables.tf
@@ -6,22 +6,26 @@ variable "environ" {
 variable "vpc_cidr" {
   type        = string
   description = "The IP range to use for the VPC"
+  default = "10.0.0.0/16"
 }
 
-variable "av_zones" {
-  type = list(string)
-  description = "AZs to create subnets into"
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
-variable "public_subnets" {
-  type = list(string)
-  description = "subnets to create for public network traffic, one per AZ"
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.6.0/24"]
 }
 
-#variable "private_subnets" {
- # type = list(string)
- # description = "subnets to create for private network traffic, one per AZ"
-#}
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b"]
+}
 
 variable "vpc_subnet_count" {
   type = number
